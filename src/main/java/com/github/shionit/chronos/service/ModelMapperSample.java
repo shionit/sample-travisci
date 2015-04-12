3e6e78f2dba6570b5c3b604ca570cac243107e2d
shionit <frontier.engine@gmail.com>
2015-04-12 18:23:52 +0900

    add some tests.

--- src/main/java/com/github/shionit/chronos/service/ModelMapperSample.java
+++ src/main/java/com/github/shionit/chronos/service/ModelMapperSample.java
@@ -3,14 +3,40 @@ package com.github.shionit.chronos.service;
 import com.github.shionit.chronos.model.Order;
 import com.github.shionit.chronos.model.OrderDto;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 
 /**
  * Created by itoshion on 2015/04/10.
  */
 public class ModelMapperSample {
 
-    public OrderDto convert(Order order) {
-        ModelMapper modelMapper = new ModelMapper();
+    private ModelMapper modelMapper = new ModelMapper();
+
+    public ModelMapper getModelMapper() {
+        return modelMapper;
+    }
+
+    public OrderDto convertNew(Order order) {
         return modelMapper.map(order, OrderDto.class);
+    }
+
+    public void convert(Order order, OrderDto dto) {
+        modelMapper.map(order, dto);
+    }
+
+    public void prepareMapper() {
+        modelMapper.addMappings(new PropertyMap<Order, OrderDto>() {
+            @Override
+            protected void configure() {
+                map().setBillingCity(source.getBillingAddress().getCity());
+                map().setBillingStreet(source.getBillingAddress().getStreet());
+                map().setCustomerFirstName(source.getCustomer().getName().getFirstName());
+                map().setCustomerLastName(source.getCustomer().getName().getLastName());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
     }
 }
--- src/test/java/com/github/shionit/chronos/service/ModelMapperSampleTest.java
+++ src/test/java/com/github/shionit/chronos/service/ModelMapperSampleTest.java
@@ -10,27 +10,48 @@ import static org.junit.Assert.assertEquals;
  */
 public class ModelMapperSampleTest {
 
+    public static final int MAX_COUNT = 1000000;
     private ModelMapperSample target = new ModelMapperSample();
 
     @Test
     public void testConvert() {
-        Name name = new Name();
-        name.setFirstName("Taro");
-        name.setLastName("Yamada");
-        Customer customer = new Customer();
-        customer.setName(name);
-        Address address = new Address();
-        address.setCity("Tokyo");
-        address.setStreet("Central-Way");
-        Order order = new Order();
-        order.setBillingAddress(address);
-        order.setCustomer(customer);
+        Order order = createTestOrder();
 
-        OrderDto result = target.convert(order);
+        OrderDto result = target.convertNew(order);
 
         assertEquals(order.getCustomer().getName().getFirstName(), result.getCustomerFirstName());
         assertEquals(order.getCustomer().getName().getLastName(), result.getCustomerLastName());
         assertEquals(order.getBillingAddress().getStreet(), result.getBillingStreet());
         assertEquals(order.getBillingAddress().getCity(), result.getBillingCity());
+    }
+
+    private static final Order createTestOrder() {
+        final Name name = new Name();
+        name.setFirstName("Taro");
+        name.setLastName("Yamada");
+        final Customer customer = new Customer();
+        customer.setName(name);
+        final Address address = new Address();
+        address.setCity("Tokyo");
+        address.setStreet("Central-Way");
+        final Order order = new Order();
+        order.setBillingAddress(address);
+        order.setCustomer(customer);
+        return order;
+    }
+
+    @Test
+    public void testMapPerformance() {
+        Order order = createTestOrder();
+        OrderDto result;
+
+        long start = System.nanoTime();
+
+        for (int i = 0; i < MAX_COUNT; i++) {
+            result = target.convertNew(order);
+        }
+
+        long end = System.nanoTime();
+        System.out.println("testMapPerformance():" + String.valueOf(end - start));
     }
 }
